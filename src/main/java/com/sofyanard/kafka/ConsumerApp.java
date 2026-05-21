d4064f2806561108d84a8443c4b5e9df84133469
Sofyan Ardianto <sofyanard@gmail.com>
2026-05-21 15:31:47 +0700

    connect to confluent cloud :shark:

--- src/main/java/com/sofyanard/kafka/ConsumerApp.java
+++ src/main/java/com/sofyanard/kafka/ConsumerApp.java
@@ -8,29 +8,64 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-public class ConsumerApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+@Component
+@Profile("consumer")
+public class ConsumerApp implements CommandLineRunner {
 
-        Properties appProps = new Properties();
-        try (var input = ConsumerApp.class.getClassLoader().getResourceAsStream("application.properties")) {
-            appProps.load(input);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
+    @Value("${kafka.auto-offset-reset}")
+    private String autoOffsetReset;
+
+    @Value("${kafka.key-deserializer}")
+    private String keyDeserializer;
+
+    @Value("${kafka.value-deserializer}")
+    private String valueDeserializer;
+
+    @Value("${kafka.consumer-group-id}")
+    private String consumerGroupId;
+
+    @Value("${kafka.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${kafka.sasl.mechanism}")
+    private String saslMechanism;
+
+    @Value("${kafka.sasl.username}")
+    private String saslUsername;
+
+    @Value("${kafka.sasl.password}")
+    private String saslPassword;
+
+    @Value("${kafka.topic}")
+    private String topic;
+
+    @Override
+    public void run(String... args) throws Exception {
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, appProps.getProperty("kafka.bootstrap-servers"));
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, appProps.getProperty("kafka.auto-offset-reset"));
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, appProps.getProperty("kafka.key-deserializer"));
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                appProps.getProperty("kafka.value-deserializer"));
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, appProps.getProperty("kafka.group-id"));
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        properties.put("security.protocol", securityProtocol);
+        properties.put("sasl.mechanism", saslMechanism);
+        properties.put(
+                "sasl.jaas.config",
+                String.format(
+                        "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";",
+                        saslUsername,
+                        saslPassword));
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(List.of("thermostat_readings"));
+        consumer.subscribe(List.of(topic));
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
